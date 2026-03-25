5670779f960b92cfa1a0216ae277ea4dc1d46190
Ali Ramoul <ali@ramsys-school.dz>
2026-03-25 22:25:56 +0100

    Force deploy: datetime fix

--- app.py
+++ app.py
@@ -1781,3 +1781,4 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
 # Force redeploy Wed Mar 25 19:34:15 WAT 2026
 # redeploy Wed Mar 25 21:45:18 WAT 2026
+# force deploy Wed Mar 25 22:25:56 WAT 2026
